eb0673a0b8692914cf98f76ed71f8fcdea5bd1e1
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-04-10 06:27:37 +0200

    Delete a.sh

--- a.sh
+++
@@ -1,4 +0,0 @@
-curl 'https://canary.discord.com/api/v9/applications/1223421353907064913/public' \
-  -H 'authorization: NTI1MzE2MzkzNzY4NDUyMDk4.G9UpDY.kIdX3rDD_XUkURlcJSTp_-GnmTaM8gFaAWvjRE' \
-  -H 'content-type: application/json' \
-  --compressed | jq
